ac0a97c6d1efc08a19f020f0f0253ff78f602bea
Darren Cheng <darren@thanx.com>
2017-11-17 20:20:14 -0800

    Add Shoryuken::Subscriber.

--- lib/aws-broker.rb
+++ lib/aws-broker.rb
@@ -2,3 +2,9 @@ require 'aws-sdk-sqs'
 require 'aws-sdk-sns'
 require 'aws/broker'
 require 'aws/broker/railtie' if defined?(Rails)
+
+begin
+  require 'shoryuken'
+  require 'shoryuken/subscriber'
+rescue LoadError
+end
